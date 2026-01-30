3214742b1ab95513c07593d172b48943c3149db2
Anupam khosla <anupamkhosla1993@gmail.com>
2026-01-30 20:32:15 +0530

    Add changes to complete my Upsun configuration

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -897,3 +897,8 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+// Upsun configuration
+if (getenv('PLATFORM_APPLICATION') && file_exists(__DIR__ . '/settings.upsun.php')) {
+  include __DIR__ . '/settings.upsun.php';
+}
