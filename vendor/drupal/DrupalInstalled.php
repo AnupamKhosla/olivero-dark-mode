2e8d31cced9c8376a809e6b62953f153db8fe885
Anupam khosla <anupamkhosla1993@gmail.com>
2026-02-01 16:03:51 +0530

    ddev-upsun experimental combo to sync upsun cloud containers
    trying to sync remote cloud upsun conteianers exactly on local to sync them fully

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '4fd2e5d7336a1e4a';
+  public const string VERSIONS_HASH = 'ef3af0cc2d468c84';
 
 }
