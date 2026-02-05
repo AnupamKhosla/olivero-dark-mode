ace759cb1de60cde68e8bc41dcd9492553cf9622
Anupam khosla <anupamkhosla1993@gmail.com>
2026-02-05 22:00:28 +0530

    Major changes to gulfile logic

--- README.md
+++ README.md
@@ -26,3 +26,64 @@ It effectively functions as a **"Living Documentation"** — developers can clon
 * Bootstrap
 * DDEV local environment
 * Custom sub-theme of Barrio
+
+
+###Common Commands
+
+ddev ssh
+cd /var/www/html/web/themes/custom/custom_bootstrap_sass
+
+### Important workflow customizations:
+
+Gulp -- hide bootstrap errors: <addLinkOfBsOfficialSuggestionToIgnoreSassErrors>
+
+quietDeps: true,  // <--- ADD THIS LINE (Silences dependency warnings)
+silenceDeprecations: ['import', 'global-builtin', 'color-functions']
+logger: dartSass.Logger.silent, // hides *all* sass warnings
+
+Gulp -- running from inside the container -- **EXPERIMENTAL**
+
+By default, if you run gulp from inside the docker container, the localhost:3000 is not accessible via the browser. You could run npx gulp from outside the container(without ddev ssh) as a temporary fix, but the idea is, we wanna use docker container's exact node, npm and gulp versions. E.g., you might be using two drupal projects one using Drupal 9 with Gulp v4. Then you'd need to manually adjust nvm etc to use correct gulp for individual projects. The whole point of docker is to circumvent that issue.
+
+So, the solution is either manually expose port 3000 of the container to our browser or use ddev-browserSYnc addon(not working):
+
+
+ddev add-on get ddev/ddev-browsersync -- Not working yet. This prolly spins up it's own browserSync server.
+
+
+Or, 
+
+.ddev/config.yaml
+
+web_extra_exposed_ports:
+  - name: browsersync
+    container_port: 3000
+    http_port: 3000
+    https_port: 3001
+
+Then in gulpfile:
+
+function serve(done) {
+  browserSync.init({
+    proxy: 'http://localhost',     
+    listen: '0.0.0.0',
+    port: 3000,
+    ui: false,
+    open: false,    
+    online: false,
+    socket: {
+        domain: 'dp.ddev.site:3001'
+    }    
+  }, done);
+
+  gulp
+    .watch([paths.scss.watch, paths.scss.bootstrap], styles)
+    .on('change', browserSync.reload);
+  gulp.watch(paths.scss.componentsWatch, createCssComponent);
+}
+
+We basically bind yourname.ddev.site:3001 to docker web container's internal localhost url exposed from gulp. But, then we start to see problem of login forms and other forms action value being wrong. Drupal redirects to localhost based url from php.
+
+I think the sacrifice of using global Gulp is worth it. We can simply manually use nvm or other techniques like gulp4 and gulp3 binaries.
+
+Another thing is running gulp from outside the container works, but but when we submit a form, e.g., login it takes you to yoursite.ddev.site url not the localhost url. Regardless, if someone wants to use Gulp from inside the container they can use gulpfile_docker.js
---
+++ web/themes/custom/custom_bootstrap_sass/gulpfile.js
@@ -0,0 +1,233 @@
+//Run gulp and visit yoursite.ddev.com:3001 to see changes live.
+
+// Modified by Anupam khosla 5 Feb 2026 from origina barrio theme. 
+
+// If you want to run Gulp  inside the docker container.
+
+// make sure your ddev config contains 
+
+// web_extra_exposed_ports:
+//   - name: browsersync
+//     container_port: 3000
+//     http_port: 3000
+//     https_port: 3001
+
+// https://dp.ddev.site:3001 -- replace with https://yoursite.ddev.site:3001
+
+// may be redundant to have scriptPath function below.
+
+// Sass warnings have been silenced to avoid confusion with warnings from dependencies.
+// If you want to see sass warnings, remove the 'logger: dartSass.Logger.silent' line
+// and the 'silenceDeprecations' array and quietDeps: true, from the sass options in both the styles and createCssComponent functions.
+
+
+const DDEV_HOSTNAME = 'dp.ddev.site';
+const HTTPS_PORT    = '3001';
+
+let gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  sourcemaps = require('gulp-sourcemaps'),
+  $ = require('gulp-load-plugins')(),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  postcssInlineSvg = require('postcss-inline-svg'),
+  browserSync = require('browser-sync').create(),
+  pxtorem = require('postcss-pxtorem'),
+  postcssProcessors = [
+    postcssInlineSvg({
+      removeFill: true,
+      paths: ['./node_modules/bootstrap-icons/icons'],
+    }),
+    pxtorem({
+      propList: [
+        'font',
+        'font-size',
+        'line-height',
+        'letter-spacing',
+        '*margin*',
+        '*padding*',
+      ],
+      mediaQuery: true,
+    }),
+  ];
+
+
+const dartSass = require('sass');
+
+
+const paths = {
+  scss: {
+    src: './scss/style.scss',
+    dest: './css',
+    watch: './scss/**/*.scss',
+    bootstrap: './node_modules/bootstrap/scss/bootstrap.scss',
+    components: './components/**/*.scss',
+    componentsWatch: './components/**/*.scss',
+  },
+  js: {
+    bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
+    barrio: '../../contrib/bootstrap_barrio/js/barrio.js',
+    dest: './js',
+  },
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function styles() {
+  return gulp
+    .src([paths.scss.bootstrap, paths.scss.src])
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        includePaths: [
+          './node_modules/bootstrap/scss',
+          '../../contrib/bootstrap_barrio/scss',
+        ],
+        quietDeps: true,  // <--- (Silences dependency warnings)
+        silenceDeprecations: [ // <--- (Silences dependency warnings)
+                'legacy-js-api', 
+                'mixed-decls', 
+                'color-functions', 
+                'global-builtin', 
+                'import', 
+                'slash-div'
+              ],
+        logger: dartSass.Logger.silent, // hides *all* sass warnings
+      }).on('error', sass.logError)
+    )
+    .pipe($.postcss(postcssProcessors))
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: [
+            'Chrome >= 35',
+            'Firefox >= 38',
+            'Edge >= 12',
+            'Explorer >= 10',
+            'iOS >= 8',
+            'Safari >= 8',
+            'Android 2.3',
+            'Android >= 4',
+            'Opera >= 12',
+          ],
+        }),
+      ])
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(browserSync.stream());
+}
+
+function createCssComponent(fileWithLocation) {
+  return gulp
+    .src([paths.scss.components])
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+        quietDeps: true,
+        silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions', 'mixed-decls'],
+        logger: dartSass.Logger.silent, // hides *all* sass warnings
+      }).on('error', sass.logError)
+    )
+    .pipe($.postcss(postcssProcessors))
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: [
+            'Chrome >= 35',
+            'Firefox >= 38',
+            'Edge >= 12',
+            'Explorer >= 10',
+            'iOS >= 8',
+            'Safari >= 8',
+            'Android 2.3',
+            'Android >= 4',
+            'Opera >= 12',
+          ],
+        }),
+      ])
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./components/' + '.'))
+    .pipe(cleanCss())
+    .pipe(browserSync.stream());
+}
+
+// Move the javascript files into our js folder
+function js() {
+  return gulp
+    .src([paths.js.bootstrap, paths.js.popper, paths.js.barrio])
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(browserSync.stream());
+}
+
+// Static Server + watching scss/html files
+
+function serve(done) {
+  browserSync.init({
+    proxy: {
+      target: 'http://localhost',
+
+      reqHeaders: {
+        'Host': DDEV_HOSTNAME,
+        'X-Forwarded-Host': DDEV_HOSTNAME,
+        'X-Forwarded-Proto': 'https',
+        'X-Forwarded-Port': HTTPS_PORT,
+        // 1. DISABLE COMPRESSION (Critical for Injection)
+        // If Drupal sends GZIP content, BrowserSync can't read/inject the script.
+        'Accept-Encoding': 'identity' 
+      },
+
+      proxyRes: [function (proxyRes, req, res) {
+        if (proxyRes.headers.location) {
+          var original = proxyRes.headers.location;
+          var safeHost = DDEV_HOSTNAME.replace(/\./g, '\\.');
+          var regex = new RegExp('https?:\/\/(localhost|' + safeHost + ')(:\\d+)?');
+          var replacement = 'https://' + DDEV_HOSTNAME + ':' + HTTPS_PORT;
+          proxyRes.headers.location = original.replace(regex, replacement);
+        }
+      }]
+    },
+
+    // 2. SOCKET CONFIGURATION (Restored)
+    // explicitly tells the browser to use the Secure URL for the socket
+    socket: {
+        domain: 'https://' + DDEV_HOSTNAME + ':' + HTTPS_PORT
+    },
+
+    listen: '0.0.0.0',
+    port: 3000,
+    ui: false,
+    open: false,    
+    online: false,
+    logLevel: "silent"
+
+  }, function(err, bs) {
+    console.log('\n');
+    console.log('\x1b[32m%s\x1b[0m', '---------------------------------------------------');
+    console.log('\x1b[36m%s\x1b[0m', ' DDEV BrowserSync Ready!');
+    console.log(' Access URL: \x1b[35mhttps://' + DDEV_HOSTNAME + ':' + HTTPS_PORT + '\x1b[0m');
+    console.log('\x1b[32m%s\x1b[0m', '---------------------------------------------------');
+    console.log('\n');
+    done();
+  });
+
+  gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload);
+  gulp.watch(paths.scss.componentsWatch, createCssComponent);
+}
+
+// ... exports ...
+
+const build = gulp.series(styles, gulp.parallel(js, serve));
+
+exports.styles = styles;
+exports.js = js;
+exports.serve = serve;
+
+exports.default = build;
